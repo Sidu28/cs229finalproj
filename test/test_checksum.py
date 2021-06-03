f303f995b8728b788e3d25c08fd2dd36ada8fce1
Siddhartha Jagannath <60323455+Sidu28@users.noreply.github.com>
2021-06-02 22:57:11 -0400

    Delete test directory

--- test/test_checksum.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-sys.path.append('../')
-from features.checksum import ChecksumExtractor
-
-
-def test_checksum():
-    extractor = ChecksumExtractor('data/goodware_example')
-    assert ((list(extractor.extract().values())[0]) > 0 )
--- test/test_import_info.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-sys.path.append('../')
-from features.import_info import ImportInfoExtractor
-
-
-def test_import_info():
-    extractor = ImportInfoExtractor('data/goodware_example')
-    assert (type(list(extractor.extract().values())[0]) == str)
